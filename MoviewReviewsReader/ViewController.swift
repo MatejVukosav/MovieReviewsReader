c3686f39729688261eda46c10d73d0fbded26ca9
Matej <vuki146@gmail.com>
2017-02-26 13:26:32 +0100

    Initial Commit

---
+++ MoviewReviewsReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MoviewReviewsReader
+//
+//  Created by user on 2/26/17.
+//  Copyright © 2017 vuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
